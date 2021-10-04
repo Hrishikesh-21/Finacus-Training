32e4ea32f767f3e8814427f0fb5852716b9c6a0e
Hrishikesh-21 <91135114+Hrishikesh-21@users.noreply.github.com>
2021-10-04 20:10:46 +0530

    Add files via upload

---
+++ React Js/Day 5/src/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./index.css";
+
+
+
+
+function Navbar(){
+    return(
+        <>
+        <div className="Titlee"> &#9883;NETFLIX  </div>
+        
+
+<div className="navbar">
+<ul>
+  <li><a href="https://www.netflix.com/signup/regform">&#9883; Log In</a></li>
+  <li><a href="https://www.netflix.com/signup">&#9883; Sign UP</a></li>
+  <li><a href="https://www.netflix.com/signup/planform">&#9883; Netflix Plans</a></li>
+  
+
+</ul>
+</div>
+</>
+    );
+}
+
+export default Navbar;
